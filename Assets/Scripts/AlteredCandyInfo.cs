61296b174bcb8838aa0a534825a4f5d680192a18
Jay Mehta <jay.mehta@kwalee.com>
2024-08-15 23:52:05 +0530

    Upload the project

---
+++ Assets/Scripts/AlteredCandyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class AlteredCandyInfo
+{
+    private List<GameObject> newCandy { get; set; } = new();
+    public int maxDistance { get; set; }
+
+    /// <summary>
+    /// Returns distinct list of altered candy
+    /// </summary>
+    public IEnumerable<GameObject> alteredCandy => newCandy.Distinct();
+
+    public void AddCandy(GameObject go)
+    {
+        if (!newCandy.Contains(go))
+        {
+            newCandy.Add(go);
+        }
+    }
+}
